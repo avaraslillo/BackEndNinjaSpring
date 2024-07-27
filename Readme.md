922be858d2863bae71bbd6c1a6a3a06670d5d820
avaraslillo <a.varaslillo@hotmail.com>
2024-07-27 16:01:23 -0400

    Corrección en configuración de seguridad

---
+++ Readme.md
@@ -0,0 +1,3 @@
+########Aplicación de registro de usuarios#############
+
+Esta aplicación permite registrar usuarios con sus respectivos nombres, apellidos, teléfonos y ciudades.
--- src/main/java/com/udemy/backendninja/configuration/SecurityConfiguration.java
+++ src/main/java/com/udemy/backendninja/configuration/SecurityConfiguration.java
@@ -8,21 +8,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -62,7 +54,7 @@ public class SecurityConfiguration {
           formLogin
               .loginPage("/login").loginProcessingUrl("/logincheck")
               .usernameParameter("username").passwordParameter("password")
-              .defaultSuccessUrl("/loginsuccess")
+              .defaultSuccessUrl("/loginsuccess",true)
               .permitAll()
       )
       .logout(logout ->
